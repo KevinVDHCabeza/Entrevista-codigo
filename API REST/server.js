a6b6e811d5c22e2bb6150d606cae45acdc764f85
KevinVDHCabeza <kevin@fingerscrossed.io>
2024-06-06 10:28:34 +0200

    [+] Added swagger

--- API REST/server.js
+++ API REST/server.js
@@ -2,18 +2,24 @@ const express = require('express');
 const userRoutes = require('./routes/users');
 const houseRoutes = require('./routes/houses');
 const { inicializarBaseDatos } = require('./db/database');
-const app = express();
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 const PORT = process.env.PORT || 3000;
 
-// Configura middleware para manejar solicitudes JSON
+const app = express();
+
+
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 app.use(express.json());
 
 inicializarBaseDatos();
 
-// Montar las rutas de usuarios en la raíz de la aplicación
 app.use('/users', userRoutes);
 
-// Montar las rutas de casas en la raíz de la aplicación
+
 app.use('/houses', houseRoutes);
 
 // Iniciar el servidor
